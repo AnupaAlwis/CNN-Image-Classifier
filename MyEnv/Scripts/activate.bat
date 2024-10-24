a1908bbf8cd77c71185866831e3df66a311825ca
Sanuda Amasith <sanuda.ats@gmail.com>
2024-10-24 08:42:20 +0530

    split the dataset

--- MyEnv/Scripts/activate.bat
+++ MyEnv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Education\UoM\Semester 5\EN3150 - Pattern Recognition\Assignments\Assignment 03\CNN-Image-Classifier\MyEnv
+set VIRTUAL_ENV=E:\Education\CNN-Image-Classifier\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(MyEnv) %PROMPT%
+set PROMPT=(myenv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(MyEnv) 
+set VIRTUAL_ENV_PROMPT=(myenv) 
 
 :END
 if defined _OLD_CODEPAGE (
